98b4bcd02afbab9b802c752736fc9e97ff38b5ee
xiaohuo <xiaohuo200@gmail.com>
2023-11-30 12:06:26 +0800

    feat: set fee collector address

---
+++ scripts/setFeeCollector.ts
@@ -0,0 +1,69 @@
+import { ethers } from 'ethers'
+import { L3Config } from './l3ConfigType'
+import { abi as ArbOwner__abi } from '@arbitrum/nitro-contracts/build/contracts/src/precompiles/ArbOwner.sol/ArbOwner.json'
+import fs from 'fs'
+import { ethOrERC20Deposit } from './nativeTokenDeposit'
+import { createERC20Bridge } from './createTokenBridge'
+import { l3Configuration } from './l3Configuration'
+import { defaultRunTimeState, RuntimeState } from './runTimeState'
+import { transferOwner } from './transferOwnership'
+// Delay function
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function checkRuntimeStateIntegrity(rs: RuntimeState) {
+  if (!rs.chainId) {
+    rs.chainId = defaultRunTimeState.chainId
+  }
+  if (!rs.etherSent) {
+    rs.etherSent = defaultRunTimeState.etherSent
+  }
+  if (!rs.nativeTokenDeposit) {
+    rs.nativeTokenDeposit = defaultRunTimeState.nativeTokenDeposit
+  }
+  if (!rs.tokenBridgeDeployed) {
+    rs.tokenBridgeDeployed = defaultRunTimeState.tokenBridgeDeployed
+  }
+  if (!rs.l3config) {
+    rs.l3config = defaultRunTimeState.l3config
+  }
+  if (!rs.transferOwnership) {
+    rs.transferOwnership = defaultRunTimeState.transferOwnership
+  }
+}
+
+async function main() {
+  // Read the environment variables
+  const privateKey = ''
+  const L3_RPC_URL = 'http://localhost:8449'
+  const networkFeeReceiver = ''
+  if (!privateKey || !L3_RPC_URL) {
+    throw new Error('Required environment variable not found')
+  }
+
+  // ArbOwner precompile setup
+  const arbOwnerABI = ArbOwner__abi
+
+  // Generating providers from RPCs
+  const L3Provider = new ethers.providers.JsonRpcProvider(L3_RPC_URL)
+  const l3signer = new ethers.Wallet(privateKey).connect(L3Provider)
+
+  // Arb Owner precompile address
+  const arbOwnerAddress = '0x0000000000000000000000000000000000000070'
+
+  const ArbOwner = new ethers.Contract(arbOwnerAddress, arbOwnerABI, l3signer)
+  // Set the network fee receiver
+  const tx = await ArbOwner.setNetworkFeeAccount(networkFeeReceiver)
+  // Wait for the transaction to be mined
+  const recepit = await tx.wait()
+  console.log(
+    `network fee receiver is set on the block number ${await recepit.blockNumber} on the Orbit chain`
+  )
+}
+
+// Run the script
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
