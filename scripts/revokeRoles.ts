36a572c21ab120214504d643dbad30ad3dd11f4c
xiaohuo <xiaohuo200@gmail.com>
2023-11-30 18:36:32 +0800

    feat: grant and revoke roles for upgrade executor

---
+++ scripts/revokeRoles.ts
@@ -0,0 +1,85 @@
+import { ethers } from 'ethers'
+import L1AtomicTokenBridgeCreator from '@arbitrum/token-bridge-contracts/build/contracts/contracts/tokenbridge/ethereum/L1AtomicTokenBridgeCreator.sol/L1AtomicTokenBridgeCreator.json'
+import UpgradeExecutor from '@arbitrum/nitro-contracts/build/contracts/src/mocks/UpgradeExecutorMock.sol/UpgradeExecutorMock.json'
+import { config as dotenvConfig } from 'dotenv'
+import { resolve } from 'path'
+import {
+  TOKEN_BRIDGE_CREATOR_Arb_Goerli,
+  TOKEN_BRIDGE_CREATOR_Arb_Sepolia,
+} from './createTokenBridge'
+import { getSigner } from './erc20TokenBridgeDeployment'
+
+dotenvConfig({ path: resolve(__dirname, '../.env') })
+
+async function main() {
+  // Read the environment variables
+  const privateKey = process.env.PRIVATE_KEY
+  const L2_RPC_URL = process.env.L2_RPC_URL
+  const L3_RPC_URL = process.env.L3_RPC_URL
+  const newOwner = process.env.NewOwner
+  if (!privateKey || !L3_RPC_URL || !newOwner) {
+    throw new Error('Required environment variable not found')
+  }
+
+  // Generating providers from RPCs
+  const L2Provider = new ethers.providers.JsonRpcProvider(L2_RPC_URL)
+  const L3Provider = new ethers.providers.JsonRpcProvider(L3_RPC_URL)
+  const l3Deployer = getSigner(L3Provider, privateKey)
+  const l2Deployer = getSigner(L2Provider, privateKey)
+
+  //fetching chain id of parent chain
+  const l2ChainId = (await L2Provider.getNetwork()).chainId
+  const l3ChainId = (await L3Provider.getNetwork()).chainId
+
+  let TOKEN_BRIDGE_CREATOR: string
+  if (l2ChainId === 421613) {
+    TOKEN_BRIDGE_CREATOR = TOKEN_BRIDGE_CREATOR_Arb_Goerli
+  } else if (l2ChainId === 421614) {
+    TOKEN_BRIDGE_CREATOR = TOKEN_BRIDGE_CREATOR_Arb_Sepolia
+  } else {
+    throw new Error(
+      'The Base Chain you have provided is not supported, please put RPC for Arb Goerli or Arb Sepolia'
+    )
+  }
+
+  const L1AtomicTokenBridgeCreator__factory = new ethers.Contract(
+    TOKEN_BRIDGE_CREATOR,
+    L1AtomicTokenBridgeCreator.abi,
+    l2Deployer
+  )
+  const l1TokenBridgeCreator =
+    L1AtomicTokenBridgeCreator__factory.connect(l2Deployer)
+
+  const executorContractAddress =
+    await l1TokenBridgeCreator.getCanonicalL2UpgradeExecutorAddress(l3ChainId)
+  //Defining upgrade executor contract
+  const executorContract__factory = new ethers.Contract(
+    executorContractAddress,
+    UpgradeExecutor.abi,
+    l3Deployer
+  )
+  const upgradeExecutor = executorContract__factory.connect(l3Deployer)
+  const adminRole = await upgradeExecutor.ADMIN_ROLE()
+  const executorRole = await upgradeExecutor.EXECUTOR_ROLE()
+
+  console.log('admin role: ', adminRole)
+  console.log('executor role: ', executorRole)
+
+  // grant both roles to the new owner
+  const receipt = upgradeExecutor.revokeRole(adminRole, newOwner).wait()
+  console.log(
+    'Transaction complete, grant admin role on TX:',
+    receipt.transactionHash
+  )
+  const receipt2 = upgradeExecutor.revokeRole(executorRole, newOwner).wait()
+  console.log(
+    'Transaction complete, grant executor role on TX:',
+    receipt2.transactionHash
+  )
+}
+
+// Run the script
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
