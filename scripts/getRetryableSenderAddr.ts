b72db27f49a80e22619f90267e95ac22564eedd8
xiaohuo <xiaohuo200@gmail.com>
2023-12-08 13:51:08 +0800

    feat: write to file

--- scripts/getRetryableSenderAddr.ts
+++ scripts/getRetryableSenderAddr.ts
@@ -1,6 +1,7 @@
 import { ethers } from 'ethers'
 import L1AtomicTokenBridgeCreator from '@arbitrum/token-bridge-contracts/build/contracts/contracts/tokenbridge/ethereum/L1AtomicTokenBridgeCreator.sol/L1AtomicTokenBridgeCreator.json'
 import { getSigner } from './erc20TokenBridgeDeployment'
+import fs from 'fs'
 import {
   TOKEN_BRIDGE_CREATOR_Arb_Goerli,
   TOKEN_BRIDGE_CREATOR_Arb_Sepolia,
@@ -16,7 +17,7 @@ async function main() {
   const l2Deployer = getSigner(l2Provider, PRIVATE_KEY)
   //fetching chain id of parent chain
   const l2ChainId = (await l2Provider.getNetwork()).chainId
-
+  const addInfo = new Map<string, string>()
   let TOKEN_BRIDGE_CREATOR: string
   if (l2ChainId === 421613) {
     TOKEN_BRIDGE_CREATOR = TOKEN_BRIDGE_CREATOR_Arb_Goerli
@@ -38,7 +39,13 @@ async function main() {
     L1AtomicTokenBridgeCreator__factory.connect(l2Deployer)
   // fetching retryable sender address
   const retryableSenderAddress = await l1TokenBridgeCreator.retryableSender()
-  console.log('retryable sender address: ', retryableSenderAddress)
+  addInfo.set('Address', retryableSenderAddress)
+  console.log(addInfo)
+  const info = {
+    Address: retryableSenderAddress,
+  }
+  const addressInfo = JSON.stringify(info)
+  fs.writeFileSync('./output/retryableAddress.json', addressInfo)
 }
 
 // Run the script
