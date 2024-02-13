e9a96abf560e5a4b46e86ae05d3c3fe13379a722
xiaohuo <xiaohuo@altresear.ch>
2024-02-13 13:57:57 +0800

    fix: abi for setup functions (#8)

--- scripts/setL1PricePerUnit.ts
+++ scripts/setL1PricePerUnit.ts
@@ -5,15 +5,11 @@ import { getExecutorAddress } from './getExecutorAddress'
 
 export async function main() {
   const privateKey = process.env.PRIVATE_KEY || ''
-  const L3_RPC_URL = process.env.L3_RPC_URL
+  const L2_RPC_URL = process.env.L2_RPC_URL || ''
+  const L3_RPC_URL = process.env.L3_RPC_URL || ''
+  const TOKEN_BRIDGE_CREATOR = process.env.TOKEN_BRIDGE_CREATOR || ''
+  const INBOX = process.env.INBOX || ''
   const L1_PRICE_PER_UNIT = process.env.L1_PRICE_PER_UNIT
-  const L2_RPC_URL =
-    process.env.L2_RPC_URL || 'https://arbitrum-one.alt.technology'
-  const TOKEN_BRIDGE_CREATOR =
-    process.env.TOKEN_BRIDGE_CREATOR ||
-    '0x2f5624dc8800dfA0A82AC03509Ef8bb8E7Ac000e'
-  const INBOX =
-    process.env.INBOX || '0x1285D6cE3604D341b29ccfF300d043af1CDb57e3'
   // Generating providers from RPCs
   const L2Provider = new ethers.providers.JsonRpcProvider(L2_RPC_URL)
   const L3Provider = new ethers.providers.JsonRpcProvider(L3_RPC_URL)
@@ -21,18 +17,8 @@ export async function main() {
   // Creating the wallet and signer
   const l3signer = new ethers.Wallet(privateKey).connect(L3Provider)
 
-  // ArbOwner precompile setup
-  const arbOwnerABI = ArbOwner__abi
-
   // Arb Owner precompile address
   const arbOwnerAddress = '0x0000000000000000000000000000000000000070'
-  const ArbOwner = new ethers.Contract(arbOwnerAddress, arbOwnerABI, l3signer)
-
-  // Call the isChainOwner function and check the response
-  const isSignerChainOwner = await ArbOwner.isChainOwner(l3signer.address)
-  if (!isSignerChainOwner) {
-    throw new Error('The address you have provided is not the chain owner')
-  }
 
   const upgradeExecutorProxy = await getExecutorAddress(
     TOKEN_BRIDGE_CREATOR,
@@ -48,23 +34,21 @@ export async function main() {
     l3signer
   )
   const upgradeExecutor = executorContract__factory.connect(l3signer)
+  console.log(
+    'Executing setL1PricePerUnit through the UpgradeExecutor contract'
+  )
+  console.log(`Setting L1 base fee estimate on L3 to ${L1_PRICE_PER_UNIT}`)
 
   // Constructing call data for setting L1 basefee on L3
-  const arbOwnerInterface = new ethers.utils.Interface(ArbOwner.abi)
+  const arbOwnerInterface = new ethers.utils.Interface(ArbOwner__abi)
   const targetCallData = arbOwnerInterface.encodeFunctionData(
     'setL1PricePerUnit',
     [L1_PRICE_PER_UNIT]
   )
-  console.log('Executing removeChainOwner through the UpgradeExecutor contract')
-
-  console.log(`Setting L1 base fee estimate on L3 to ${L1_PRICE_PER_UNIT}`)
   const receipt = await (
     await upgradeExecutor.executeCall(arbOwnerAddress, targetCallData)
   ).wait()
-  console.log(
-    'Transaction complete, rollup owner removed from chain owners on TX:',
-    receipt.transactionHash
-  )
+  console.log('Transaction complete on TX:', receipt.transactionHash)
   console.log('All things done! Enjoy your Orbit chain. LFG 🚀🚀🚀🚀')
 }
 
