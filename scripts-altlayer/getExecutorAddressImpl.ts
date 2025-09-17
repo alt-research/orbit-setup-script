5dfbe16e8714750b3b7b69b25327eb114c11c5e9
Antonio Nunez <antonio@altresear.ch>
2025-09-17 12:13:18 +0800

    feat: update getExecutorAddress for Muster

--- scripts-altlayer/getExecutorAddressImpl.ts
+++ scripts-altlayer/getExecutorAddressImpl.ts
@@ -7,25 +7,32 @@ export async function getExecutorAddress(
   l2Provider: ethers.providers.JsonRpcProvider,
   l3Provider: ethers.providers.JsonRpcProvider
 ) {
-  const L1AtomicTokenBridgeCreator__factory = new ethers.Contract(
-    tokenBridgeCreator,
-    L1AtomicTokenBridgeCreator.abi,
-    l2Provider
-  )
-  const l1TokenBridgeCreator =
-    L1AtomicTokenBridgeCreator__factory.connect(l2Provider)
+  // Muster is on older contracts version, cannot use the conventional way to get the UpgradeExecutor
+  if (inbox == "0x18BB8310E3a3DF4EFcCb6B3E9AeCB8bE6d4af07f") {
+    const upgradeExecutorProxy = "0x302210D41e5AEf43DB392FAD8382440f33cA0440"
+    console.log('executor proxy:', upgradeExecutorProxy)
+    return upgradeExecutorProxy
+  } else {
+    const L1AtomicTokenBridgeCreator__factory = new ethers.Contract(
+      tokenBridgeCreator,
+      L1AtomicTokenBridgeCreator.abi,
+      l2Provider
+    )
+    const l1TokenBridgeCreator =
+      L1AtomicTokenBridgeCreator__factory.connect(l2Provider)
 
-  //fetching L3 upgrade executor address
-  const upgradeExecutorProxy = (
-    await l1TokenBridgeCreator.inboxToL2Deployment(inbox)
-  ).upgradeExecutor
-  console.log('executor proxy:', upgradeExecutorProxy)
+    //fetching L3 upgrade executor address
+    const upgradeExecutorProxy = (
+      await l1TokenBridgeCreator.inboxToL2Deployment(inbox)
+    ).upgradeExecutor
+    console.log('executor proxy:', upgradeExecutorProxy)
 
-  let upgradeExecutor = await l3Provider.getStorageAt(
-    upgradeExecutorProxy,
-    '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
-  )
-  upgradeExecutor = upgradeExecutor.replace(/^(0x)0+((\w{4})+)$/, '$1$2')
-  console.log('executor:', upgradeExecutor)
-  return upgradeExecutorProxy
+    let upgradeExecutor = await l3Provider.getStorageAt(
+      upgradeExecutorProxy,
+      '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+    )
+    upgradeExecutor = upgradeExecutor.replace(/^(0x)0+((\w{4})+)$/, '$1$2')
+    console.log('executor:', upgradeExecutor)
+    return upgradeExecutorProxy
+  }
 }
